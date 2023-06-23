2d70f4556da0fe25f9ef003c759b1c7d8ddfa7c9
Sergey <serega.tiuriuhanov94@gmail.com>
2023-06-23 17:06:59 +0300

    updated code, added tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+
+
+@pytest.fixture
+def test_data():
+    return Item("Смартфон", 30000, 10)
+
+
+def test_calculate_total_price(test_data):
+    assert test_data.calculate_total_price() == 300000
